06ec853fcdfc6974158bd14d3e1ab9f785bdc348
EmilyMcGonagle <emily.mcgonagle@outlook.com>
2018-11-15 10:36:10 +0000

    1.2

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -5,14 +5,14 @@ import { SocialIcon } from 'react-social-icons';
 const Contact = () =>{
     return(
         <div>
-            <fade top>
+            <Fade top>
             <h2>Contact</h2>
             <div className="contact">
                 <SocialIcon url="https://www.linkedin.com/in/emily-mcgonagle-8a9258150/" />
                 <SocialIcon url="https://github.com/EmilyMcGonagle" />
                 <p><b>Email: </b>Emily.mcgonagle@accenture.com</p>
             </div>
-            </fade>
+            </Fade>
         </div>
     );
 }
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
-
+import Fade from 'react-reveal/Fade';
 const Footer= () => {
     return(
         <div className="footer">
+        <Fade top>
             <p><b>&copy; 2018 Emily McGonagle</b></p>
-            <p>Created Using React.JS, Node.JS</p>
+            <p>Created Using React.JS, Node.JS</p></Fade>
         </div>
         );
     
